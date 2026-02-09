32dfb401685327a7ac77e5011999e4795ffcd5cd
pts-rictjo <richard.tjornhammar@pts.se>
2026-02-09 18:31:54 +0100

    Revise celestial types and add licensing details
    Updated celestial types and added copyright information.

--- src/satellitic/constants.py
+++ src/satellitic/constants.py
@@ -1,11 +1,26 @@
+lic_ = """
+   Copyright 2026 Richard Tjörnhammar
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 import numpy as np
 
 celestial_types = {
-'Planet'    : 1 ,
 'Star'      : 0 ,
-'Moon'      : 3 ,
-'Satellit'  : 4 ,
-'Other'     : 5 }
+'Planet'    : 1 ,
+'Moon'      : 2 ,
+'Satellit'  : 3 ,
+'Other'     : 4 }
 
 universal_constants = { 'G' : 6.674 * 10**(-11)	, # m3/(kg s2)	# gravitational constant
 		'c' : 2.9979 * 10**8		, # m/s		# speed of light
@@ -52,3 +67,269 @@ constants_solar_system = { 'G' : 6.674 * 10**(-11)	, # m3/(kg s2)	# gravitationa
     'DLEO' : 1000e3		, # [ m ]
 		# Hubble constant (H0) 	 approximately 20 km/s per million light-years, or approximately 70 km/s per megaparsec
 	}
+
+
+class Celestial( object ):
+    def __init__( self ) :
+        self.atmosphere_ = None
+        self.mass_ = None
+        self.radius_ = None
+        self.surface_temperature_ = None
+        self.composition_ = None
+        self.name_ = None
+#
+# TOPOLOGY INFORMATION
+class Starsystem ( object ) :
+    def __init__( self , constants ) :
+        self.name_= None
+        self.components_ = None
+        self.celestial_bodies_ = None
+        self.celestial_types_  = None
+        self.celestial_names_  = None
+        self.indices_ = dict()
+        self.constants_ = constants
+        self.Ncurrent_ = None
+        self.t_ = None
+        self.s_ = None
+        #
+        # Phase space
+        self.r_ = None
+        self.v_ = None
+        self.m_ = None
+
+    def planets( self ) :
+        if not self.celestial_types_ is None :
+            plnt_idx = np.where( self.celestial_types_ == 'Planet' )
+            return self.celestial_names_[plnt_idx],plnt_idx
+
+    def make_arrays( self , xp = np ):
+        if self.r_ is None or self.v_ is None or self.m_ is None :
+            print('Warning: Could not retrieve phase space')
+        else :
+            self.r_ = xp.asarray(self.r_)
+            self.v_ = xp.asarray(self.v_)
+            self.m_ = xp.asarray(self.m_)
+            self.celestial_names_ = xp.asarray(self.celestial_names_)
+
+    def assign_from_dict( self , dictionary , xp=np ) :
+        i = 0
+        for item in dictionary.items() :
+            if len(item[1]) != 5 :
+                print("Error in starsystem data format")
+                exit(1)
+
+            if self.celestial_names_ is None :
+                self.celestial_names_ = xp.asarray( [ item[0] ] ) 
+            else :
+                self.celestial_names_ = xp.hstack( [self.celestial_names_ , item[0]] )
+            self.indices_[item[0]] = i
+            i = i+1
+
+            if self.r_ is None :
+                self.r_ = xp.asarray( [item[1][0],0,0] )
+            else :
+                self.r_ = xp.vstack([self.r_, [item[1][0],0,0] ])  
+
+            if self.s_ is None :
+                self.s_ = xp.asarray([ item[1][1] ])
+            else :
+                self.s_ = xp.hstack( [self.s_, item[1][1]] )
+
+            if self.t_ is None :
+                self.t_ = xp.asarray([ item[1][2] ])
+            else :
+                self.t_ = xp.hstack( [self.t_, item[1][2]] )
+
+            if item[1][2] > 0 :
+                v = item[1][0] * 2 * np.pi / item[1][2]
+            else :
+                v = 0
+
+            if self.v_ is None :
+                self.v_ = xp.asarray( [0,v,0] )
+            else :
+                self.v_ = xp.vstack([self.v_, [0,v,0] ]) 
+
+            if self.m_ is None :
+                self.m_ = xp.asarray([ item[1][3] ])
+            else :
+                self.m_ = xp.hstack( [self.m_, item[1][3]] )
+
+            if self.celestial_types_ is None :
+                self.celestial_types_ = [ item[1][4] ]
+            else :
+                self.celestial_types_ .append( item[1][4] )
+            
+            self.Ncurrent_ = len(self.m_)
+            self.make_arrays(xp)
+
+    def create_name_index(self,name):
+        if self.celestial_names_ is None :
+            print('error: must specify celestial_names_')
+            return
+        idx_ = np.where(self.celestial_names_==planet)[0][0]
+        if self.indices_ is None :
+            self.indices_ = dict()                 
+            self.indices_[name] = idx_ 
+        elif not name in self.indices_ :
+            self.indices_[name] = idx_
+        return idx_
+
+    def phase_space(self, name=None):
+        if self.r_ is None or self.v_ is None or self.m_ is None :
+            print('Warning: Could not retrieve phase space')
+            return None
+        elif name is None :
+            return self.r_, self.v_, self.m_
+        else :
+            idx_ = None
+            if not self.indices_ is None :
+                if name in self.indices_ :
+                    idx_ = self.indices_[name]
+            if idx_ is None :
+                idx_ = self.create_name_index( name )
+            return self.r_[idx_], self.v_[idx_], self.m_[idx_]
+
+    def phase_state(self):
+        return self.r_, self.v_, self.m_, self.celestial_types_, self.celestial_names_
+
+    def add_particles(self , r_new, v_new, m_new, types_new , names_new ):
+        self.r_ = np.vstack( [self.r_ , r_new] )
+        self.v_ = np.vstack( [self.v_ , v_new] )
+        self.m_ = np.hstack( [self.m_ , m_new] )
+        self.celestial_types_ = np.hstack( [self.celestial_types_, types_new] )
+        self.celestial_names_ = np.hstack( [self.celestial_names_, names_new] )
+        if self.indices_ is None :
+            self.indices_ = dict()
+        for i,name in zip( range(self.Ncurrent_ , len(m_new) + self.Ncurrent_ ),
+                            names_new ):
+            self.indices_[name] = i
+        self.Ncurrent_ = len( self.m_ )
+        
+    def celestial_types(self):
+        return self.celestial_types_
+
+    def constants( self , sel = None ) :
+        if sel is None :
+            return ( self.constants_ )
+        else :
+            return ( self.constants_[sel] )
+
+    def find_indices_of( self, partial_name ):
+        return ( np.where( np.strings.find( self.celestial_names_ , partial_name ) == 0 )[0] )
+
+
+class TLESatellites ( object ) :
+    def __init__( self , tle_file_name=None , planet=None ,xp=np , tag='LEO' ) :
+        self.components_    = None
+        self.tle_file_name_ = tle_file_name
+        self.planet_        = planet
+        self.r_ = None
+        self.v_ = None
+        self.m_ = None
+        self.tag_ = tag
+        self.names_ = None
+        self.types_ = None
+        self.assign()
+        self.generate_nametypes()
+        self.make_arrays(xp)
+
+    def satellite_masses(self, N, default=1000.0) :
+        return np.full(N, default) #.reshape(-1,1)
+
+    def generate_nametypes(self, head=None, xp=np ):
+        if head is None :
+            head = self.tag_
+        self.names_ = []
+        self.types_ = []
+        for i in range(len(self.m_)):
+            self.names_.append( head+str(i) )
+            self.types_.append( celestial_types['Satellit'] )
+        self.types_ = xp.asarray(self.types_)
+        self.names_ = xp.asarray(self.names_) #.reshape(-1,1)
+
+    def assign(self , date = [2025,1,1] ):
+        if not self.tle_file_name_ is None :
+            from datetime import datetime
+            from tle_io import read_tles, tles_to_states
+            #
+            epoch = datetime( *date )
+            sats				= read_tles( self.tle_file_name_ )
+            r_sat, v_sat, names	= tles_to_states(sats, epoch)
+            m_sat   = self.satellite_masses(len(r_sat))
+            self.r_ = r_sat
+            self.v_ = v_sat
+            self.m_ = m_sat
+        else:
+            print ( 'error: you need to specify .set_tle(tle_file_name)')
+
+    def add_planet_dependency(self, name, r_planet, v_planet ):
+        self.r_ += r_planet
+        self.v_ += v_planet
+        if self.planet_ is None :
+            self.planet_ = name
+        elif not self.planet_ == name :
+            print ( 'error: planet missmatch', planet, self.planet_ )
+        
+    def set_tle(self,tle_file_name):
+        self.tle_file_name_ = tle_file_name
+
+    def make_arrays( self , xp = np ):
+        if self.r_ is None or self.v_ is None or self.m_ is None :
+            print('Warning: Could not retrieve phase space')
+        else :
+            self.r_ = xp.asarray(self.r_)
+            self.v_ = xp.asarray(self.v_)
+            self.m_ = xp.asarray(self.m_)
+
+    def phase_space(self):
+        if self.r_ is None or self.v_ is None or self.m_ is None :
+            print('Warning: Could not retrieve phase space')
+            return None
+        else :
+            return self.r_, self.v_, self.m_
+
+    def phase_state(self):
+        return self.r_, self.v_, self.m_, self.types_, self.names_
+
+    def block_indices(self,Ncurrent):
+            Nsat			= len(self.m_)
+            idx_leo			= np.array(range( Ncurrent, Ncurrent+Nsat ))
+            return idx_leo
+
+class Constellation ( object ) :
+    def __init__( self  ) :
+        self.components_ = None
+
+
+AU = universal_constants['AU']
+d2s = 24*60*60 # days to secs
+solarsystem_notes = """https://www.jpl.nasa.gov/_edu/pdfs/scaless_reference.pdf"""
+solarsystem = { # Distance, Radius, revolution time, mass, type
+'Sun'      : [0        , 1391400e3*0.5, 0 , 1.989*10**30 , celestial_types['Star'] ] ,
+'Mercury'  : [0.39*AU  , 4879e3*0.5   , 87.97 *d2s , 3.285e23 , celestial_types['Planet'] ] ,
+'Venus'    : [0.72*AU  , 12104e3*0.5  , 224.7 *d2s , 4.867e24 , celestial_types['Planet'] ] ,
+'Earth'    : [1*AU     , 12756e3*0.5  , 365.25*d2s , 5.972e24 , celestial_types['Planet'] ] ,
+'EarthMoon': [AU + 384400e3, 3474e3*0.5, 29.5*d2s, 7.346*10**22 , celestial_types['Moon'] ] ,
+'Mars'     : [1.52*AU  , 6792e3*0.5   , 687.00*d2s , 6.390e23 , celestial_types['Planet'] ] ,
+'Jupiter'  : [5.20*AU  , 142984e3*0.5 , 4333.0*d2s , 1.898e27 , celestial_types['Planet'] ] ,
+'Saturn'   : [9.54*AU  , 120536e3*0.5 , 10756 *d2s , 5.683e26 , celestial_types['Planet'] ] ,
+'Uranus'   : [19.2*AU  , 51118e3*0.5  , 30687 *d2s , 8.681e25 , celestial_types['Planet'] ] ,
+'Neptune'  : [30.06*AU , 49528e3*0.5  , 60190 *d2s , 1.024e26 , celestial_types['Planet'] ] }
+
+if __name__=='__main__' :
+    print ( 'HERE' )
+    solsystemet = Starsystem( constants_solar_system )
+    solsystemet .assign_from_dict( solarsystem )
+    print ( solsystemet.phase_space() )
+    print ( solsystemet.celestial_types() )
+    earth_satellites = TLESatellites( tle_file_name = "/home/rictjo/Downloads/local_tles_smaller.txt" ,
+                                planet = 'Earth' )
+    print ( *solsystemet.phase_space(name = 'Earth')[:2] )
+    earth_satellites.add_planet_dependency( 'Earth' , *solsystemet.phase_space(name = 'Earth')[:2] )
+    print ( earth_satellites.phase_space() )
+    print ( earth_satellites.phase_state() )
+    solsystemet.add_particles( *earth_satellites.phase_state() )
+    print( solsystemet.phase_state() )
+    what='LEO'
+    print( what, solsystemet.find_indices_of( what ) )
