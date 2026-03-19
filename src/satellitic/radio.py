b0b750a174be1b4168136943874c15f0f744a841
Richard Tjörnhammar <richard.tjornhammar@pts.se>
2026-03-19 11:03:40 +0100

    Update radio.py

--- src/satellitic/radio.py
+++ src/satellitic/radio.py
@@ -1,5 +1,5 @@
 lic_ = """
-   Copyright 2025 Richard Tjörnhammar
+   Copyright 2026 Richard Tjörnhammar
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -42,3 +42,115 @@ def reciever_noise( T_K , BW_Hz ) :
     RX_N0	= 10*np.log10( kB * T_K / 1E-3 )	# mW [dBm]
     NRx		= RX_N0 + 10*np.log10(BW_Hz)		
     return ( NRx )
+
+def pfd_aggregation_kernel(
+    sat_pos_ecef ,        # (Nsat, 3)
+    gs_pos_ecef  ,        # (Ngs, 3)
+    eirp_dbw ,            # (Nsat,)
+    Nco = 80 ,
+    xp = np               # np eller cupy
+) :
+    """
+    Placeholder, första iterationen
+    Returnerar agg PFD (dBW/m^2) per ground point
+    """
+    #
+    # Senare även slant och atmo etc
+    # --- vectorized distance ---
+    # (Ngs, Nsat, 3)
+    diff = sat_pos_ecef[None, :, :] - gs_pos_ecef[:, None, :]
+    d2 = xp.sum(diff**2, axis=2)  # (Ngs, Nsat)
+    #
+    # --- Förluster
+    #
+    # --- PFD (dB) ---
+    # Todo fixme
+    # eirp_eff = eirp_dbw + tx_gain(theta_off_axis)
+    pfd_db = eirp_eff[None, :] - 10 * xp.log10(4 * xp.pi * d2)
+    #
+    # Todo fixme
+    mask = elevation > 0
+    pfd_db = xp.where(mask, pfd_db, -1e9)
+   
+    # --- välj top Nco ---
+    # partition är snabbare än sort
+    idx = xp.argpartition(pfd_db, -Nco, axis=1)[:, -Nco:]
+
+    # samla
+    top_pfd = xp.take_along_axis(pfd_db, idx, axis=1)
+
+    # --- summera linjärt ---
+    top_lin = xp.power(10.0, top_pfd / 10.0)
+    pfd_agg_lin = xp.sum(top_lin, axis=1)
+
+    pfd_agg_db = 10 * xp.log10(pfd_agg_lin)
+
+    return pfd_agg_db
+
+
+def run_interference_mc(
+    traj,                  # memmap: (Nt, Nsat, 3)
+    particle_type,
+    ground_lat,
+    ground_lon,
+    eirp_dbw,
+    fs_noise_dbw,
+    Nco=80,
+    Nmc=1000,
+    xp=np,
+    sample_gs=200,
+    bMap=True
+):
+    """
+    Returnerar I/N samples
+    """
+
+    Nt, Nsat, _ = traj.shape
+
+    if not bMap :
+        # --- välj slumpmässiga mark punkter ---
+        idx_gs = np.random.choice(len(ground_lat), sample_gs, replace=False)
+    else :
+        idx_gs = range(len(ground_lat))
+
+    gs_lat = ground_lat[idx_gs]
+    gs_lon = ground_lon[idx_gs]
+
+    # Todo fixme
+    gs_pos = latlon_to_ecef(gs_lat, gs_lon)  # (Ngs,3)
+
+    IN_samples = []
+
+    for k in range(Nmc):
+
+        # --- slumpa tid ---
+        t = np.random.randint(0, Nt)
+
+        sat_pos = traj[t]  # (Nsat,3)
+
+        # --- kernel ---
+        pfd_agg = pfd_aggregation_kernel(
+            sat_pos,
+            gs_pos,
+            eirp_dbw,
+            Nco=Nco,
+            xp=xp
+        )
+
+        # --- FS antenna gain ---
+        # Todo fixme x2
+        theta = compute_offaxis_angles(gs_pos, sat_pos)  # approx
+        G_rx = fs_antenna_gain(theta)
+
+        # approx: ta max eller medel
+        G_eff = xp.mean(G_rx, axis=1)
+
+        # --- interferens ---
+        I_dbw = pfd_agg + G_eff
+
+        # --- I/N ---
+        IN = I_dbw - fs_noise_dbw
+
+        IN_samples.append(IN)
+
+    return xp.concatenate(IN_samples)
